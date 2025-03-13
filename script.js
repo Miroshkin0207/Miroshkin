7151b3523fc562a0688077443f00e3e2ed2408b2
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-03-13 20:47:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function najatie() {
+    let nadpis = document.getElementById("nadpis")
+    if (nadpis.innerHTML==="") {
+        nadpis.innerHTML="Молодец!"
+}else if (nadpis.innerHTML==="Молодец!") {
+        nadpis.innerHTML=""
+    }
+}
