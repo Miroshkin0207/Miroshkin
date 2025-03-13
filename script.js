4a4b97da233bad00218a5fd909fe37a8b37051df
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-03-13 21:24:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function najatie() {
+    let nadpis = document.getElementById("nadpis")
+    let knopka = document.getElementById("knopka")
+    if (nadpis.innerHTML==="") {
+    knopka.innerHTML="Вернуть"
+    nadpis.innerHTML="Молодец!"
+    }else if (nadpis.innerHTML==="Молодец!") {
+    knopka.innerHTML="Нажми"
+    nadpis.innerHTML=""
+    }
+}
