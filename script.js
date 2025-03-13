8238bbecb3ab9045d94c366c40473301f3ecd58e
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-03-13 21:24:08 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function najatie() {
-    let nadpis = document.getElementById("nadpis")
-    if (nadpis.innerHTML==="") {
-    nadpis.innerHTML="Молодец!"
-    }else if (nadpis.innerHTML==="Молодец!"){
-        nadpis.innerHTML=""
-    }
-}
